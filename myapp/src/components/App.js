8c5f6a32244ecf88b484ddcfad5498e22422a8e4
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-02 15:51:09 -0700

    feat: refactor context obj

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -22,18 +22,10 @@ class App extends React.Component {
         </div>
         {/* this is where english or dutch is rendered */}
         {/* LanguageContext.Provider is how you update value of LanguageContext dynamically as user clicks */}
-        {/* 1. Provider pipe data from state data */}
+        {/* Provider pipe data from state data */}
         <LanguageContext.Provider value={this.state.language} >
           <UserCreate />
         </LanguageContext.Provider>
-
-        {/* 2. Provider pipe data with static value dutch */}
-        <LanguageContext.Provider value="dutch" >
-          <UserCreate />
-        </LanguageContext.Provider>
-
-        {/* 3. Get data from State default value with no provider attached*/}
-        <UserCreate />
       </div>
     );
   }
--- myapp/src/contexts/LanguageContext.js
+++ myapp/src/contexts/LanguageContext.js
@@ -1,8 +1,12 @@
 import React from 'react';
 // this is context object
 // passing default value 'English'
+/*
 const context = React.createContext('english');
 
 console.log(context);
 
 export default context;
+*/
+
+export default React.createContext('english');
