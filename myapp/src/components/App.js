a3688cc87b9de80e23f22d30fa0740b20f67147d
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-02 15:43:36 -0700

    feat: three different way to get data in app.js

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends React.Component {
   state = { 
@@ -20,6 +21,18 @@ class App extends React.Component {
           <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
         {/* this is where english or dutch is rendered */}
+        {/* LanguageContext.Provider is how you update value of LanguageContext dynamically as user clicks */}
+        {/* 1. Provider pipe data from state data */}
+        <LanguageContext.Provider value={this.state.language} >
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        {/* 2. Provider pipe data with static value dutch */}
+        <LanguageContext.Provider value="dutch" >
+          <UserCreate />
+        </LanguageContext.Provider>
+
+        {/* 3. Get data from State default value with no provider attached*/}
         <UserCreate />
       </div>
     );
--- myapp/src/contexts/LanguageContext.js
+++ myapp/src/contexts/LanguageContext.js
@@ -1,4 +1,8 @@
 import React from 'react';
 // this is context object
 // passing default value 'English'
-export default React.createContext('english');
+const context = React.createContext('english');
+
+console.log(context);
+
+export default context;
