5b385fa21c62fd27656467ba27521ca498de52a1
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-02 13:22:03 -0700

    feat: render english or dutch on screen

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { 
+    language: 'english' 
+  };
+
+  onLanguageChange = language => {
+    // abbreviate since key, value the same
+    this.setState({ language });
+  }; 
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+        </div>
+        {/* this is where english or dutch is rendered */}
+        {this.state.language}
+      </div>
+    );
   }
 }
 
